72bc5e3ebb7fe98a23f50e467216754819b15765
Valery Fremaux <valery.fremaux@gmail.com>
2013-01-11 20:59:50 +0100

    First Moodle 1.9 code load

---
+++ profilefield/lang/en_utf8/enrol_profilefield.php
@@ -0,0 +1,17 @@
+<?php // $Id: enrol_manual.php,v 1.1.1.1 2010/06/11 09:25:49 vf Exp $ 
+      // enrol_manual.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['description'] = 'This method allows direct enrolment in course if user has a profile field set to the expected value';
+$string['enrolname'] = 'Profile Field Enrolment';
+$string['enrol_profilefield_fieldid'] = 'User profile field';
+$string['enrol_profilefield_value'] = 'Expected value';
+$string['badprofile'] = 'You may be disapointed, but your profile information forbids you enrolling in this course. However, if you have a good reason to be here, please contact an administrator who will alter your profile consequently.';
+$string['enrolmentconfirmation'] = 'Be welcome. Your profile information allows you to enroll in this course. Proceed ? ';
+$string['noprofileenroledcourses'] = 'No course is being enrolled based on profile fields';
+$string['fieldid'] = 'Profile field';
+$string['fieldvalue'] = 'Expected value';
+$string['course'] = 'Course : $a';
+$string['defaultvalues'] = 'Default values';
+$string['coursebound'] = 'Local course overrides';
+
+?>
---
+++ profilefield/lang/fr_utf8/enrol_profilefield.php
@@ -0,0 +1,17 @@
+<?php // $Id: enrol_manual.php,v 1.1.1.1 2010/06/11 09:25:49 vf Exp $ 
+      // enrol_manual.php - created with Moodle 1.7 beta + (2006101003)
+
+$string['description'] = 'Cette méthode permet une inscription directe si un champ du profil utilisateur contient une valeur attendue';
+$string['enrolname'] = 'Inscription basée sur le profil utilisateur';
+$string['enrol_profilefield_fieldid'] = 'Champ du profil utilisateur';
+$string['enrol_profilefield_value'] = 'Valeur attendue';
+$string['badprofile'] = 'Désolé, mais votre profil ne vous permet pas d\'accéder à ce cours. Si vous devez y entrer pour une raison légitime, contactez les administrateurs de la plate-forme qui modifieront votre profil de façon adéquate.';
+$string['enrolmentconfirmation'] = 'Bienvenue. votre profil vous autorise la participation à ce cours. Voulez-vous vous inscrire ? ';
+$string['noprofileenroledcourses'] = 'Aucun cours inscrit à partir des champs de profil';
+$string['fieldid'] = 'Champ du profil';
+$string['fieldvalue'] = 'Valeur attendue';
+$string['course'] = 'Cours : $a';
+$string['defaultvalues'] = 'Valeurs par défaut';
+$string['coursebound'] = 'Surcharges locales des cours';
+
+?>
