000090c43a403546c3f683613f394e63e8ff47a2
valery <valery.fremaux@gmail.com>
2014-02-14 11:35:18 -0800

    fix required version to Moodle 2.4

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2013011100;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2011112900;        // Requires this Moodle version
+$plugin->requires  = 2012120308;        // Requires this Moodle version
 $plugin->component = 'enrol_profilefield';  // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_RC;
-$plugin->release = "2.2.0 (Build 2013011100)";
+$plugin->release = "2.4.0 (Build 2013011100)";
