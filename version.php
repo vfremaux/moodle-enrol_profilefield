795544a1d717d75771835679a1cc9dd293b18bf4
valery <valery.fremaux@gmail.com>
2016-09-28 13:01:40 +0200

    automated sync from SVN

--- version.php
+++ version.php
@@ -20,7 +20,7 @@
  * @package    enrol_profilefield
  * @category   enrol
  * @author     Valery Fremaux <valery.fremaux@gmail.com>
- * @copyright  2012 Valery Fremaux
+ * @copyright  2012 Valery Fremaux (http://www.mylearningfactory.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
