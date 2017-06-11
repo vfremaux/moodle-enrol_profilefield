dcfb2f064caa942001f4cda6c6601c35a832d0f0
valery <valery.fremaux@gmail.com>
2017-06-11 13:59:35 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2014052200; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2016052300; // Requires this Moodle version.
+$plugin->requires  = 2016112900; // Requires this Moodle version.
 $plugin->component = 'enrol_profilefield'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.1.0 (Build 2014052200)';
+$plugin->release = '3.2.0 (Build 2014052200)';
 
 // Non Moodle attributes.
-$plugin->codeincrement = '3.1.0000';
+$plugin->codeincrement = '3.2.0000';
